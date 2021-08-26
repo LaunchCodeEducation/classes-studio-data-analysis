c27c81fab5ad637f595b6e6aa2ed55b73301145f
Sally Steuterman <sally.steuterman@gmail.com>
2021-08-26 14:00:07 -0500

    Added starter code

---
+++ main.py
@@ -0,0 +1,11 @@
+# Declare a class called CrewCandidate with an __init__ that takes three parameters—name, mass, and scores. Note that scores will be an list of test results.
+
+
+  
+def main():
+  # Create objects, call the class and methods, and add print statements here.
+
+  print("Hello, cleaning robots!")
+
+if __name__ == '__main__':
+  main()
